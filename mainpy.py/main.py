c26eb8c8f042dc00fbd11712ce1e0c1d2748425c
ibo shoka <i.shoka09152@gmail.com>
2024-05-04 19:20:00 +0500

    exercise

--- mainpy.py/main.py
+++ mainpy.py/main.py
@@ -1,5 +1,12 @@
-tst = 5
-if tst > 10:
-    print("True")
-else:
-    print("Folse")
+# tst = -5
+# if tst < 0 and tst > -10 and tst > -8 and tst < 30:
+# #if tst > 10 and tst < 20:
+#     print("True")
+# else:
+#     print("False")
+
+# tst =['a','b','c','d','e']
+# if len(tst) < 6 and len(tst) > 0:
+#     print("true")
+# else:
+#     print("false")
